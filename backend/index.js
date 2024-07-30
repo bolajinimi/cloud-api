b1a203eac683e60d3251fa4cfe96f5853539bb2e
Olatunji Clement <clement.olatunji@decagon.dev>
2024-07-30 09:04:47 +0100

    API created for sending pdf to whatsapp number with Twillio

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const twilio = require('twilio');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
-const accountSid = 'ACaafc2e6ff5c0184acc427964f8437272'; 
-const authToken = 'f1c23138e56b930885feb61c0a5d5175';  
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = new twilio(accountSid, authToken);
 
 app.post('/send-pdf', async (req, res) => {
